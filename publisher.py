100fff5c8b8f311755bb91f6b714b001d7296103
Gabriel Fajardo <gfajardo95@gmail.com>
2021-02-16 07:54:43 -0500

    change comment

--- publisher.py
+++ publisher.py
@@ -5,7 +5,7 @@ from google.cloud import pubsub_v1
 import settings
 
 
-# publishes the tweets to a Pub/Sub topic
+# publishes the country sentiments to a Pub/Sub topic
 def publish(lines):
     client = pubsub_v1.PublisherClient()
     topic_path = client.topic_path(settings.PROJECT_NAME, settings.PUBSUB_TOPIC_NAME)
